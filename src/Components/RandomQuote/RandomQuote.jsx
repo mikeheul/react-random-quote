0c304df0ba760a9e65c1856eb14541a13f4699b7
Micka <mickael.murmann@gmail.com>
2023-09-12 14:41:16 +0200

    init commit

---
+++ src/Components/RandomQuote/RandomQuote.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import './RandomQuote.css'
+import twitter_icon from '../Assets/twitter_icon.png'
+import reload_icon from '../Assets/reload_icon.png'
+
+export const RandomQuote = () => {
+
+    let quotes = [];
+    async function loadQuotes() {
+        const response = await fetch("https://type.fit/api/quotes");
+        quotes = await response.json();
+    }
+
+    const random = () => {
+        const select = quotes[Math.floor(Math.random() * quotes.length)]
+        setQuote(select);
+    }
+
+    const twitter = () => {
+        window.open(`https://twitter.com/intent/tweet?text=${quote.text} - ${quote.author.split(", ")[0]}`);
+    }
+
+    useEffect(() => {
+        loadQuotes().then((quotes) => {
+            random();
+        });
+    }, []);
+
+    const [quote, setQuote] = useState({
+        text: "",
+        author: ""
+    });
+
+    loadQuotes();
+    
+    return (
+        <div className='container'>
+            <div className="quote">{quote.text}</div>
+            <div>
+                <div className="line"></div>
+                <div className="bottom">
+                    <div className="author">- {quote.author.split(", ")[0]}</div>
+                    <div className="icons">
+                        <img src={reload_icon} onClick={() => {random()}} alt="" />
+                        <img src={twitter_icon} onClick={() => {twitter()}} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
